518d5ce81b1ba334bb28ff65a528e887b260d3b0
Hawkins kent <hakent27@gmail.com>
2022-12-20 08:45:34 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+import math
+
+amount = int(input("What is the number of dollars you want to split? "))
+
+# can't have more than half of the total be 20s
+# val = amount / 2
+val = amount
+
+# Twentys
+val = val / 20
+val = math.floor(val)
+
+twentys = val
+
+left_over = twentys * 20
+
+val = amount - left_over
+last_val = val
+
+# Tens
+val = val / 10
+val = math.floor(val)
+
+tens = val
+
+left_over = tens * 10
+
+val = last_val - left_over
+last_val = val
+
+# Fives
+val = val / 5
+val = math.floor(val)
+
+fives = val
+
+left_over = fives * 5
+
+val = last_val - left_over
+last_val = val
+
+# Ones
+ones = val
+
+# Print it
+print("Twentys: " + str(twentys) + " Tens:" + str(tens) +  " Fives:" + str(fives) + " Ones:" + str(ones))
